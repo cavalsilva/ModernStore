7482e6a498de5ce6d6b62aa4a8b32c8df547ba5e
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-03-22 06:40:05 -0300

    Inclusão do projeto

---
+++ ModernStore.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using ModernStore.Domain.Entities;
+using System;
+
+namespace ModernStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User("cavalsilva", "cavalsilva");
+            var customer = new Customer(
+                "Ricardo",
+                "Silva", 
+                "ricardocavalcantesilva@gmail.com",
+                user);
+
+            if (!customer.IsValid())
+            {
+                foreach (var notification in customer.Notifications)
+                {
+                    Console.WriteLine(notification.Message);
+                }
+            }
+
+            Console.WriteLine(customer.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ModernStore.Domain/Entities/Customer.cs
@@ -0,0 +1,53 @@
+﻿using FluentValidator;
+using ModernStore.Shared.Entities;
+using System;
+
+namespace ModernStore.Domain.Entities
+{
+    public class Customer : Entity
+    {
+        public Customer(
+            string firstName, 
+            string lastName, 
+            string email,
+            User user)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = null;
+            Email = email;
+            User = user;
+
+
+            // Validações
+            new ValidationContract<Customer>(this)
+                .IsRequired(x => x.FirstName, "Nome é obrigatório")
+                .HasMaxLenght(x => x.FirstName, 60)
+                .HasMinLenght(x => x.FirstName, 3)
+                .IsRequired(x => x.LastName, "Sobrenome é obrigatório")
+                .HasMaxLenght(x => x.LastName, 60)
+                .HasMinLenght(x => x.LastName, 3)
+                .IsEmail(x => x.Email, "E-mail inválido");
+
+        }
+
+        public Guid Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public DateTime? BirthDate { get; private set; }
+        public string Email { get; private set; }
+        public User User { get; private set; }
+
+        public void Update(string firstName, string lastName, DateTime birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
